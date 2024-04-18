56b4030662600e2134dbf113cfd02186fe653554
hvhai <hunterx6323@gmail.com>
2024-04-18 15:37:35 +0700

    feat: animation base

--- core/src/com/codehunter/game/CommonActor.java
+++ core/src/com/codehunter/game/CommonActor.java
@@ -1,26 +1,37 @@
 package com.codehunter.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
 
 public class CommonActor extends Actor {
-    private final TextureRegion textureRegion;
+//    private final TextureRegion textureRegion;
     private final Rectangle rectangle;
+
+    private Animation<TextureRegion> animation;
+    private float elapsedTime;
+    private boolean animationPaused;
 
     public CommonActor(Texture texture, float x, float y, Stage stage) {
         super();
         rectangle = new Rectangle();
         rectangle.setSize(texture.getWidth(), texture.getHeight());
 
-        textureRegion = new TextureRegion(texture, texture.getWidth(), texture.getHeight());
-        setSize(texture.getWidth(), texture.getHeight());
+//        textureRegion = new TextureRegion(texture, texture.getWidth(), texture.getHeight());
+//        setSize(texture.getWidth(), texture.getHeight());
         setPosition(x, y);
         stage.addActor(this);
+
+        animation = null;
+        elapsedTime = 0;
+        animationPaused = false;
     }
 
     public Rectangle getRectangle() {
@@ -35,6 +46,10 @@ public class CommonActor extends Actor {
     @Override
     public void act(float delta) {
         super.act(delta);
+        if (!animationPaused)
+            elapsedTime += delta;
+//        if ( animation != null && isAnimationFinished() )
+//            remove();
     }
 
     @Override
@@ -43,11 +58,79 @@ public class CommonActor extends Actor {
 
         Color color = getColor();
         batch.setColor(color.r, color.g, color.b, color.a);
-        if (isVisible()) {
-            batch.draw(textureRegion,
+//        if (isVisible())
+        if (animation != null && isVisible())
+            batch.draw(animation.getKeyFrame(elapsedTime),
                     getX(), getY(), getOriginX(), getOriginY(),
                     getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
-        }
+//            batch.draw(textureRegion,
+//                    getX(), getY(), getOriginX(), getOriginY(),
+//                    getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+
     }
 
+    public void setAnimation(Animation<TextureRegion> animation) {
+        this.animation = animation;
+        TextureRegion animationTextureRegion = animation.getKeyFrame(0);
+        float width = animationTextureRegion.getRegionWidth();
+        float height = animationTextureRegion.getRegionHeight();
+        setSize(width, height);
+        setOrigin(width / 2, height / 2);
+    }
+
+    public Animation<TextureRegion> loadAnimationFromSheet(String filename, int rows, int cols,
+                                                           float frameDuration, boolean loop) {
+        Texture texture = new Texture(Gdx.files.internal(filename), true);
+        texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        int frameWidth = texture.getWidth() / cols;
+        int frameHeight = texture.getHeight() / rows;
+        TextureRegion[][] temp = TextureRegion.split(texture, frameWidth, frameHeight);
+        Array<TextureRegion> textureRegionArray = new Array<>();
+        for (int row = 0; row < rows; row++)
+            for (int col = 0; col < cols; col++)
+                textureRegionArray.add(temp[row][col]);
+
+        Animation<TextureRegion> ani = new Animation<>(frameDuration, textureRegionArray);
+        if (loop)
+            ani.setPlayMode(Animation.PlayMode.LOOP);
+        else
+            ani.setPlayMode(Animation.PlayMode.NORMAL);
+        if (animation == null)
+            setAnimation(ani);
+        return ani;
+    }
+
+    public Animation<TextureRegion> loadAnimationFromFiles(String[] fileNames,
+                                                           float frameDuration, boolean loop) {
+        int fileCount = fileNames.length;
+        Array<TextureRegion> textureArray = new Array<TextureRegion>();
+        for (int n = 0; n < fileCount; n++) {
+            String fileName = fileNames[n];
+            Texture texture = new Texture(Gdx.files.internal(fileName));
+            texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+            textureArray.add(new TextureRegion(texture));
+        }
+        Animation<TextureRegion> anim = new Animation<TextureRegion>(frameDuration, textureArray);
+        if (loop)
+            anim.setPlayMode(Animation.PlayMode.LOOP);
+        else
+            anim.setPlayMode(Animation.PlayMode.NORMAL);
+        if (animation == null)
+            setAnimation(anim);
+        return anim;
+    }
+
+    public Animation<TextureRegion> loadTexture(String fileName) {
+        String[] fileNames = new String[1];
+        fileNames[0] = fileName;
+        return loadAnimationFromFiles(fileNames, 1, true);
+    }
+    public boolean isAnimationFinished()
+    {
+        return animation.isAnimationFinished(elapsedTime);
+    }
+    public void setAnimationPaused(boolean pause)
+    {
+        animationPaused = pause;
+    }
 }
--- core/src/com/codehunter/game/MyGdxGame.java
+++ core/src/com/codehunter/game/MyGdxGame.java
@@ -2,6 +2,8 @@ package com.codehunter.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 
 public class MyGdxGame extends BaseGame {
 
@@ -9,20 +11,44 @@ public class MyGdxGame extends BaseGame {
     CommonActor apple;
     CommonActor winMessage;
 
+    CommonActor knight;
+    CommonActor runner;
+
     @Override
     protected void initialize() {
-        apple = new CommonActor(new Texture("apple.png"), 60, 60, mainStage);
+
+        apple = new CommonActor(new Texture("apple.png"), 100, 100, mainStage);
+        apple.loadTexture("apple.png");
+
         shark = new Shark(new Texture("shark.png"), 0, 0, mainStage);
+        shark.loadTexture("shark.png");
+        Action spin = Actions.rotateBy(180, 5);
+        shark.addAction(spin);
+        Action move = Actions.moveBy(50, 40, 2);
+        shark.addAction(move);
+        Action moveAfter = Actions.moveBy(50, 0, 2);
+        shark.addAction(Actions.after(moveAfter));
+
+
         Texture messageTexture = new Texture("win.png");
         winMessage = new CommonActor(messageTexture,
                 (float) Gdx.graphics.getWidth() / 2 - (float) messageTexture.getWidth() / 2,
                 (float) Gdx.graphics.getHeight() / 2 - (float) messageTexture.getHeight() / 2,
                 mainStage);
+        winMessage.loadTexture("win.png");
         winMessage.setVisible(false);
+
+        knight = new CommonActor(new Texture("knight-run.png"), 50, 200, mainStage);
+        knight.loadAnimationFromSheet("knight-run.png", 1, 7, 1/7f,true);
+
+        runner = new CommonActor(new Texture("sprite-animation4.png"), 200, 200, mainStage);
+        runner.loadAnimationFromSheet("sprite-animation4.png", 5, 6, 1/30f,true);
 
         apple.setDebug(true);
         shark.setDebug(true);
         winMessage.setDebug(true);
+        knight.setDebug(true);
+        runner.setDebug(true);
     }
 
 
