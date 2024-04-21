b1750a4c2d9ae2be780183d118df69f564a42aaa
hvhai <hunterx6323@gmail.com>
2024-04-21 16:40:13 +0700

    feat: add velocity and acceleration

--- core/src/com/codehunter/game/CommonActor.java
+++ core/src/com/codehunter/game/CommonActor.java
@@ -6,17 +6,26 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 
 public class CommonActor extends Actor {
+
     private final Rectangle rectangle;
 
     private Animation<TextureRegion> animation;
     private float elapsedTime;
     private boolean animationPaused;
+
+    private Vector2 velocityVector;
+    private Vector2 accelerationVector;
+    private float acceleration;
+    private float maxSpeed;
+    private float deceleration;
 
     public CommonActor(float x, float y, Stage stage) {
         super();
@@ -28,6 +37,12 @@ public class CommonActor extends Actor {
         animation = null;
         elapsedTime = 0;
         animationPaused = false;
+
+        velocityVector = new Vector2(0, 0);
+        accelerationVector = new Vector2(0,0);
+        acceleration = 0;
+        maxSpeed = 1000;
+        deceleration = 0;
     }
 
     public Rectangle getRectangle() {
@@ -42,8 +57,9 @@ public class CommonActor extends Actor {
     @Override
     public void act(float delta) {
         super.act(delta);
-        if (!animationPaused)
+        if (!animationPaused) {
             elapsedTime += delta;
+        }
     }
 
     @Override
@@ -52,10 +68,11 @@ public class CommonActor extends Actor {
 
         Color color = getColor();
         batch.setColor(color.r, color.g, color.b, color.a);
-        if (animation != null && isVisible())
+        if (animation != null && isVisible()) {
             batch.draw(animation.getKeyFrame(elapsedTime),
                     getX(), getY(), getOriginX(), getOriginY(),
                     getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+        }
     }
 
     // --------------------------------------------------
@@ -73,29 +90,33 @@ public class CommonActor extends Actor {
     }
 
     public Animation<TextureRegion> loadAnimationFromSheet(String filename, int rows, int cols,
-                                                           float frameDuration, boolean loop) {
+            float frameDuration, boolean loop) {
         Texture texture = new Texture(Gdx.files.internal(filename), true);
         texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         int frameWidth = texture.getWidth() / cols;
         int frameHeight = texture.getHeight() / rows;
         TextureRegion[][] temp = TextureRegion.split(texture, frameWidth, frameHeight);
         Array<TextureRegion> textureRegionArray = new Array<>();
-        for (int row = 0; row < rows; row++)
-            for (int col = 0; col < cols; col++)
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
                 textureRegionArray.add(temp[row][col]);
+            }
+        }
 
         Animation<TextureRegion> ani = new Animation<>(frameDuration, textureRegionArray);
-        if (loop)
-            ani.setPlayMode(Animation.PlayMode.LOOP);
-        else
+        if (loop) {
+            ani.setPlayMode(Animation.PlayMode.LOOP); 
+        }else {
             ani.setPlayMode(Animation.PlayMode.NORMAL);
-        if (animation == null)
+        }
+        if (animation == null) {
             setAnimation(ani);
+        }
         return ani;
     }
 
     public Animation<TextureRegion> loadAnimationFromFiles(String[] fileNames,
-                                                           float frameDuration, boolean loop) {
+            float frameDuration, boolean loop) {
         int fileCount = fileNames.length;
         Array<TextureRegion> textureArray = new Array<TextureRegion>();
         for (int n = 0; n < fileCount; n++) {
@@ -105,12 +126,14 @@ public class CommonActor extends Actor {
             textureArray.add(new TextureRegion(texture));
         }
         Animation<TextureRegion> anim = new Animation<TextureRegion>(frameDuration, textureArray);
-        if (loop)
-            anim.setPlayMode(Animation.PlayMode.LOOP);
-        else
+        if (loop) {
+            anim.setPlayMode(Animation.PlayMode.LOOP); 
+        }else {
             anim.setPlayMode(Animation.PlayMode.NORMAL);
-        if (animation == null)
+        }
+        if (animation == null) {
             setAnimation(anim);
+        }
         return anim;
     }
 
@@ -119,12 +142,70 @@ public class CommonActor extends Actor {
         fileNames[0] = fileName;
         return loadAnimationFromFiles(fileNames, 1, true);
     }
-    public boolean isAnimationFinished()
-    {
+
+    public boolean isAnimationFinished() {
         return animation.isAnimationFinished(elapsedTime);
     }
-    public void setAnimationPaused(boolean pause)
-    {
+
+    public void setAnimationPaused(boolean pause) {
         animationPaused = pause;
+    }
+
+    // -------------------------------------------------- 
+    // Movement
+    // --------------------------------------------------
+    public void setSpeed(float speed) {
+        if (velocityVector.len() == 0) {
+            velocityVector.set(speed, 0); 
+        }else {
+            velocityVector.setLength(speed);
+        }
+    }
+
+    public float getSpeed() {
+        return this.velocityVector.len();
+    }
+
+    public void setMotionAngle(float angle) {
+        velocityVector.setAngleDeg(angle);
+    }
+
+    public float getMotionAngle() {
+        return this.velocityVector.angleDeg();
+    }
+
+    public boolean isMoving() {
+        return (getSpeed() > 0);
+    }
+
+    public void setAcceleration(float acc) {
+        acceleration = acc;
+    }
+
+    public void accelerationAtAngle(float angle) {
+        accelerationVector.add(new Vector2(acceleration, 0).setAngleDeg(angle));
+    }
+
+    public void setMaxSpeed(float maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+    
+    public void setDeceleration(float deceleration) {
+        this.deceleration = deceleration;
+    }
+
+    public void applyPhysic(float delta) {
+        // apply acceleration
+        velocityVector.add(accelerationVector.x*delta, accelerationVector.y*delta);
+        float speed = getSpeed();
+        // decrease speed when not accelerating
+        if (accelerationVector.len() == 0)
+            speed -= deceleration*delta;
+        // keep speed within set bounds
+        speed = MathUtils.clamp(speed, 0, maxSpeed);
+        // appy velocity
+        moveBy(velocityVector.x*delta, velocityVector.y*delta);
+        // reset the acceleration
+        accelerationVector.set(0, 0);
     }
 }
--- core/src/com/codehunter/game/Runner.java
+++ core/src/com/codehunter/game/Runner.java
@@ -1,10 +1,45 @@
 package com.codehunter.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class Runner extends CommonActor {
+
     public Runner(float x, float y, Stage stage) {
         super(x, y, stage);
         loadAnimationFromSheet("sprite-animation4.png", 5, 6, 1 / 30f, true);
+
+        setAcceleration(400);
+        setMaxSpeed(100);
+        setDeceleration(400);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && getX() > 0) {
+            accelerationAtAngle(180);
+        }
+        boolean isNotMaxWidth = getX() < (Gdx.graphics.getWidth() - getWidth());
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && isNotMaxWidth) {
+            accelerationAtAngle(0);
+        }
+        boolean isNotMaxHeight = getY() < (Gdx.graphics.getHeight() - getHeight());
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) && isNotMaxHeight) {
+            accelerationAtAngle(90);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && getY() > 0) {
+            accelerationAtAngle(270);
+        }
+
+        applyPhysic(delta);
+
+        // setAnimationPaused(!isMoving());
+
+        if (getSpeed() > 0) {
+            setRotation(getMotionAngle());
+        }
     }
 }
