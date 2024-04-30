88a4102404652a62833371d74a2f9478356736c6
hvhai <hunterx6323@gmail.com>
2024-04-30 10:44:05 +0700

    chore: init space rock game

--- core/src/com/codehunter/game/screen/MenuScreen.java
+++ core/src/com/codehunter/game/screen/MenuScreen.java
@@ -9,17 +9,6 @@ import static com.codehunter.game.BaseGame.setActiveScreen;
 public class MenuScreen extends BaseScreen {
     @Override
     protected void initialize() {
-        CommonActor ocean = new CommonActor(0, 0, mainStage);
-        ocean.loadTexture("water.jpg");
-        ocean.setSize(800, 600);
-        CommonActor title = new CommonActor(0, 0, mainStage);
-        title.loadTexture("starfish-collector.png");
-        title.centerAtPosition(400, 300);
-        title.moveBy(0, 100);
-        CommonActor start = new CommonActor(0, 0, mainStage);
-        start.loadTexture("message-start.png");
-        start.centerAtPosition(400, 300);
-        start.moveBy(0, -100);
     }
 
     @Override
